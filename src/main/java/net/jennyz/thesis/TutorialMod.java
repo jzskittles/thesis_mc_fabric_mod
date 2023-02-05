ed208f76c7e75bb3453bdea707f3282b39313fe7
Jenny Zhang <jennyzhang1235@gmail.com>
2023-02-05 17:41:11 -0500

    Initial commit

---
+++ src/main/java/net/jennyz/thesis/TutorialMod.java
@@ -0,0 +1,15 @@
+package net.jennyz.thesis;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "thesis";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
